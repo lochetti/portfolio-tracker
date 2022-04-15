c951836e2db3b851a6b54c58dd0d9683b7313f4b
Renato Lochetti <renato.lochetti@gmail.com>
2022-04-15 20:23:44 +0100

    Adding connection to sqlite database and first migration creating the trades table

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
+mod db;
 mod env;
 
+use anyhow::Result;
 use axum::{
+    extract::Extension,
     http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
-use rusqlite::{Connection, Result};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    let env = env::EnvVars::load();
+
+    let pool = match db::prepare_db_and_get_connection().await {
+        Ok(pool) => pool,
+        Err(e) => {
+            println!("Error creating preparing database connection {}", e);
+            return;
+        }
+    };
+
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user));
-
-    let env = env::EnvVars::load();
-
-    test_sqlite().unwrap();
+        .route("/users", post(create_user))
+        .layer(Extension(pool));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -31,42 +40,6 @@ async fn main() {
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-}
-
-fn test_sqlite() -> Result<Person> {
-    let conn = Connection::open("portfolio_tracker.db")?;
-
-    /*     conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL
-        )",
-        [],
-    )?; */
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-    };
-    conn.execute("INSERT INTO person (name) VALUES (?1)", [&me.name])?;
-
-    let mut stmt = conn.prepare("SELECT id, name FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-    Ok(me)
 }
 
 async fn create_user(
